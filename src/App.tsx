a31df3719b8f5403d6b84e29b622304e52a67a66
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-01 22:41:42 +0200

    feat: adding state with Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 import { Children } from "./Children";
+
+const GuestList = (): JSX.Element => {
+	const [name, setName] = useState("");
+	const [list, setList] = useState<string[]>([]);
+	const handleClick = () => {
+		const newList = list.slice();
+		setList([...newList, name]);
+		setName("");
+	};
+	return (
+		<>
+			<input
+				type="text"
+				onChange={(e) => setName(e.target.value)}
+				value={name}
+			/>
+			<button type="submit" onClick={handleClick}>
+				Add
+			</button>
+			<div>
+				{list.map((inv) => (
+					<p>{inv}</p>
+				))}
+			</div>
+		</>
+	);
+};
 function App() {
 	return (
 		<div className="App">
 			<Children k={2} name="Jack">
 				zzzzzzzz
 			</Children>
+			<div>
+				<GuestList />
+			</div>
 		</div>
 	);
 }
