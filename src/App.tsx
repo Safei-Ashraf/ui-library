0f2b80c7dcd37effcec34863c8a602f1aa9612f0
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-09 01:41:29 +0200

    feat: comment unsued code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Button from "./components/Button";
+// import Button from "./components/Button";
+import StarIcon from "./components/StarIcon";
 function App() {
 	return (
 		<div className="App">
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias.
-			<Button className="">Click Me</Button>
+			{/* <Button className="">Click Me</Button> */}
+			<StarIcon size="sm" color="red" />
 		</div>
 	);
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,30 +1,35 @@
-import { ReactNode } from "react";
+import { ReactElement, ReactNode } from "react";
 // import className from "classnames";
 import "./Button.css";
-
+type IconProps = {
+	size?: "lg" | "md" | "sm";
+	color: string;
+};
 interface ButtonProps {
 	variant?: "filled" | "ghost" | "text";
-	size?: "large" | "medium" | "small";
-	icon?: ReactNode;
+	size?: "lg" | "md" | "sm";
+	renderIcon: () => ReactElement<IconProps>;
 	iconDirection: "right" | "left";
 	className?: string;
 	children?: ReactNode;
 	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 	color?: "primary" | "secondary" | "neutral";
 }
-
+const Icon = () => "Icon";
 export const Button = ({
 	variant = "filled",
-	size = "medium",
-	icon = "",
+	size = "md",
 	children,
+	renderIcon,
 	...props
 }: ButtonProps): JSX.Element => {
 	// const classes = className({
 	// 	btn: props.primary,
 	// });
+	const icon = renderIcon();
 	return (
 		<button {...props} className="bg-primary text-secondary btn">
+			{icon}
 			{children}
 		</button>
 	);
