93c08d7b45b3a20641329a227ee2df5b81890c3e
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-08 22:58:17 +0200

    feat: adjust component file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import { Button } from "./components/Button/Button";
-
+import Button from "./components/Button";
 function App() {
 	return (
 		<div className="App bg-primary-main border border-primary-border text-primary-main font-inter text-fs.40">
