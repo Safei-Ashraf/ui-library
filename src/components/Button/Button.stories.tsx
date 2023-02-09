8b4897b25c0d888e973debd7a774a24814d1f36a
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-09 15:01:53 +0200

    feat: create button story

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,1 +1,32 @@
 import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from "./Button";
+
+//default export that describes the component,
+export default {
+	title: "UI-Library/Button",
+	component: Button,
+	argTypes: {
+		variant: {
+			control: "inline-radio",
+			options: ["filled", "outline", "text"],
+		},
+		size: {
+			control: "inline-radio",
+			options: ["lg", "md", "sm"],
+		},
+		color: {
+			control: "inline-radio",
+			options: ["primary", "secondary", "neutral"],
+		},
+		iconDirection: { control: "inline-radio", options: ["left", "right"] },
+		isDisabled: { control: "boolean", type: "boolean" },
+		children: { control: "text", type: "string" },
+	},
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+// named exports that describe the stories.
+export const ButtonMain = Template.bind({});
