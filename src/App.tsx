e81fd162a90a7e7283cca2016793e54acf7a0e75
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-09 14:19:50 +0200

    feat: testing button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
+import Button from "./components/Button";
 // import Button from "./components/Button";
-import StarIcon from "./components/StarIcon";
+// import StarIcon from "./components/StarIcon";
 function App() {
 	return (
 		<div className="App">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias.
+			{/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias.
 			{/* <Button className="">Click Me</Button> */}
-			<StarIcon size="sm" color="red" />
+			{/* <StarIcon size="sm" color="red" /> */}
+
+			<Button variant="filled" color="primary" size="lg" isDisabled>
+				Click me
+			</Button>
 		</div>
 	);
 }
