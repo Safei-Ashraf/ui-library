5860326cddf41dc89c89ac3423e62c7abbbe5e94
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-20 10:08:13 +0200

    feat: add basic logic for accordion click

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { AccordionProps } from "./Accordion.d";
-export const Accordion = ({ items, ...props }: AccordionProps) => {
-	const renderedItems = items.map(({ id, heading, content }) => (
-		<li key={id}>
-			<div>{heading}</div>
-			<div>{content}</div>
-		</li>
-	));
+
+export const Accordion = ({ items, ...props }: AccordionProps): JSX.Element => {
+	const [expandedIndex, setExpandedIndex] = useState(0);
+	const handleclick = (e: React.MouseEvent<HTMLElement>): void => {
+		setExpandedIndex(+e.currentTarget.id);
+	};
+
+	const renderedItems = items.map(({ id, heading, content }, index) => {
+		const isExapnded = index === expandedIndex;
+		return (
+			<li key={id}>
+				<div onClick={handleclick} id={String(index)}>
+					{heading}
+				</div>
+				{isExapnded && <div>{content}</div>}
+			</li>
+		);
+	});
 	return <ul {...props}>{renderedItems}</ul>;
 };
