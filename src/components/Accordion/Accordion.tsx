317fafe6f597b17d069679e87ee6c374c6272f32
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-20 09:38:26 +0200

    feat: pass props

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,3 +1,10 @@
-export const Accordion = () => {
-	return <div>Accordion</div>;
+import { AccordionProps } from "./Accordion.d";
+export const Accordion = ({ items, ...props }: AccordionProps) => {
+	const renderedItems = items.map(({ id, heading, content }) => (
+		<li key={id}>
+			<div>{heading}</div>
+			<div>{content}</div>
+		</li>
+	));
+	return <ul {...props}>{renderedItems}</ul>;
 };
