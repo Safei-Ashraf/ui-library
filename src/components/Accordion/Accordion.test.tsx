a3371ac483e1fddfa85ef871fea45ac322363e98
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-20 09:38:09 +0200

    feat: add items array

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -6,14 +6,36 @@ import Accordion from "./index";
 
 //Snapshot
 describe("renders correctly", () => {
-	const tree = renderer.create(<Accordion />).toJSON();
+	const items = [
+		{
+			id: "1",
+			heading: "1st Heading here",
+			content: "1st content here 1st content here",
+		},
+		{
+			id: "2",
+			heading: "2nd Heading here",
+			content: "2nd content here 2nd content here",
+		},
+		{
+			id: "3",
+			heading: "3rd Heading here",
+			content: "3rd content here 3rd content here",
+		},
+	];
+	const tree = renderer.create(<Accordion items={items} />).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
 //Unit
 describe("Accordion", () => {
+	const items = [
+		{ id: "1", heading: "1st Heading here", content: "1st content here" },
+		{ id: "2", heading: "2nd Heading here", content: "2nd content here" },
+		{ id: "3", heading: "3rd Heading here", content: "3rd content here" },
+	];
 	it("render Accordion", () => {
-		render(<Accordion />);
+		render(<Accordion items={items} />);
 
 		expect(screen.getByText("Accordion")).toBeInTheDocument();
 	});
