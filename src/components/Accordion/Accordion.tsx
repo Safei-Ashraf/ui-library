2142f1eef386fcc6d84b31ea5adece22388289a7
Safei Ashraf <safei.ashraf@gmail.com>
2023-02-20 10:14:27 +0200

    feat: refactor click handler

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -3,15 +3,16 @@ import { AccordionProps } from "./Accordion.d";
 
 export const Accordion = ({ items, ...props }: AccordionProps): JSX.Element => {
 	const [expandedIndex, setExpandedIndex] = useState(0);
-	const handleclick = (e: React.MouseEvent<HTMLElement>): void => {
-		setExpandedIndex(+e.currentTarget.id);
-	};
 
 	const renderedItems = items.map(({ id, heading, content }, index) => {
 		const isExapnded = index === expandedIndex;
 		return (
 			<li key={id}>
-				<div onClick={handleclick} id={String(index)}>
+				<div
+					onClick={() => {
+						setExpandedIndex(index);
+					}}
+				>
 					{heading}
 				</div>
 				{isExapnded && <div>{content}</div>}
