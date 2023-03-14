93acead2b89b93b2f00f02e5e294ccad8f50a546
Safei Ashraf <safei.ashraf@gmail.com>
2023-03-14 16:47:49 +0200

    feat: rename file

--- src/components/DropDown/dummyData.ts
+++
@@ -1,7 +0,0 @@
-const items = [
-  { label: 'Title1', value: 'Option 1', id: '1', isDisabled: false },
-  { label: 'Title2', value: 'Option 2', id: '2', isDisabled: false },
-  { label: 'Title3', value: 'Option 3', id: '3', isDisabled: true },
-  { label: 'Title4', value: 'Option 4', id: '4', isDisabled: false },
-];
-export default items;
