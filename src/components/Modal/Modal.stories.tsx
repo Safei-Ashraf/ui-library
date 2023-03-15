3d9b2bd777e4321c69141112f4f154a5324419c3
Safei Ashraf <safei.ashraf@gmail.com>
2023-03-16 00:58:21 +0200

    feat: create story skeleton

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Modal } from './Modal';
+
+export default {
+  title: 'UI-Library/Modal',
+  component: Modal,
+//argTypes: {},
+} as ComponentMeta<typeof Modal>;
+
+/*
+const Template: ComponentStory<typeof modal> = args => <modal {...args} />;
+
+export const modalMain = Template.bind({});
+modalMain.args = {
+};
+*/
